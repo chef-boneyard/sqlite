3efb50f4166f04f1afefa21f7f8fe7e9082b37ed
jtimberman <joshua@opscode.com>
2012-11-26 22:21:20 -0700

    release v1.0.0

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer        "Opscode, Inc."
 maintainer_email  "cookbooks@opscode.com"
 license           "Apache 2.0"
 description       "Installs sqlite"
-version           "0.7.1"
+version           "1.0.0"
 
 recipe "sqlite", "Installs sqlite"
 
