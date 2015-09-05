08db32d49e1b86d2bda8271158a6ff4b6c0d1391
Tim Smith <tsmith84@gmail.com>
2015-09-05 15:32:08 -0700

    Add rubocop file and resolve issues

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
-name              'sqlite'
-maintainer        'Chef Software, Inc.'
-maintainer_email  'cookbooks@chef.io'
-license           'Apache 2.0'
-description       'Installs sqlite'
+name 'sqlite'
+maintainer 'Chef Software, Inc.'
+maintainer_email 'cookbooks@chef.io'
+license 'Apache 2.0'
+description 'Installs sqlite'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           '1.1.0'
+version '1.1.0'
 
 recipe 'sqlite::default', 'Installs sqlite'
 
