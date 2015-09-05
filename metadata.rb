affc979dc923381f15b71fc7a63c1ae377d89af7
Tim Smith <tsmith84@gmail.com>
2015-09-05 15:31:12 -0700

    Add long_description and fix issues_url chef 11 compat

--- metadata.rb
+++ metadata.rb
@@ -3,13 +3,14 @@ maintainer        'Chef Software, Inc.'
 maintainer_email  'cookbooks@chef.io'
 license           'Apache 2.0'
 description       'Installs sqlite'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           '1.1.0'
 
-recipe 'sqlite', 'Installs sqlite'
+recipe 'sqlite::default', 'Installs sqlite'
 
 %w(ubuntu debian centos redhat scientific oracle amazon fedora).each do |os|
   supports os
 end
 
 source_url 'https://github.com/opscode-cookbooks/sqlite' if respond_to?(:source_url)
-issues_url 'https://github.com/opscode-cookbooks/sqlite/issues' if respond_to?(:source_url)
+issues_url 'https://github.com/opscode-cookbooks/sqlite/issues' if respond_to?(:issues_url)
